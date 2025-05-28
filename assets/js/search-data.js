3e457b83e051495e3a1d63972d6f4558c23b4fb9
zilonglin <153768754+zilonglin@users.noreply.github.com>
2025-05-28 01:18:15 +0000

    Deploying to gh-pages from @ zilonglin/zilonglin.github.io@d2206ccdcea2a489fba1138b6414fc4d74efca8e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "publications by categories in reversed chronological order. (* with equal contribution)",
           section: "Navigation",
           handler: () => {
